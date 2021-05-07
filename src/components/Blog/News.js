4d2371b1a9d9d1fe3471ef732faba3026a462365
jsusilo <enr@bk.ru>
2021-05-08 00:03:20 +0500

    blog

---
+++ src/components/Blog/News.js
@@ -0,0 +1,267 @@
+import React from "react";
+
+import i1 from "../../assets/images/grid-blog-1-370x272.jpg";
+import i2 from "../../assets/images/grid-blog-2-370x272.jpg";
+import i3 from "../../assets/images/grid-blog-3-370x272.jpg";
+import i4 from "../../assets/images/grid-blog-4-370x272.jpg";
+import i5 from "../../assets/images/grid-blog-5-370x272.jpg";
+import i6 from "../../assets/images/grid-blog-6-370x272.jpg";
+
+class News extends React.Component {
+  render() {
+    return (
+      <section className="section section-inset-7 bg-default">
+        <div className="container">
+          <div className="row row-40 justify-content-center">
+            <div className="col-md-6 col-lg-4">
+              <article className="post-boxed">
+                <div className="post-meta">
+                  <div className="post-meta-item">
+                    <div className="post-author">
+                      <span>by</span> <a href="single-post.html">Martha Ryan</a>
+                    </div>
+                  </div>
+                  <div className="post-meta-item">
+                    <div className="post-date">2 days ago</div>
+                  </div>
+                </div>
+                <a className="media-wrapper" href="single-post.html">
+                  <img src={i1} alt width={370} height={272} />
+                </a>
+                <div className="post-body">
+                  <ul className="list-tags">
+                    <li>
+                      <a className="tag-1" href="single-post.html">
+                        News
+                      </a>
+                    </li>
+                  </ul>
+                  <h5 className="post-title">
+                    <a href="single-post.html">
+                      Discover Design In This Luxury Parisian Interior
+                    </a>
+                  </h5>
+                  <p className="post-exeption">
+                    Duis aliquam facilisis turpis vitae malesuada. Integer
+                    scelerisque ex non dolor tincidunt consequat. Nulla sed
+                    libero urna audire nonum.
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div className="col-md-6 col-lg-4">
+              <article className="post-boxed">
+                <div className="post-meta">
+                  <div className="post-meta-item">
+                    <div className="post-author">
+                      <span>by</span>{" "}
+                      <a href="single-post.html">Lawrence Kelly</a>
+                    </div>
+                  </div>
+                  <div className="post-meta-item">
+                    <div className="post-date">2 days ago</div>
+                  </div>
+                </div>
+                <a className="media-wrapper" href="single-post.html">
+                  <img src={i2} alt width={370} height={272} />
+                </a>
+                <div className="post-body">
+                  <ul className="list-tags">
+                    <li>
+                      <a className="tag-1" href="single-post.html">
+                        News
+                      </a>
+                    </li>
+                  </ul>
+                  <h5 className="post-title">
+                    <a href="single-post.html">
+                      Redefining Hospitality Design: A Vibrant Hotel In Rome
+                    </a>
+                  </h5>
+                  <p className="post-exeption">
+                    Vivamus fringilla accumsan nisi, non tempus lorem. Quisque
+                    non pretium risus. Maecenas pellentesque ipsum sit amet
+                    eleifend fermentum felis.
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div className="col-md-6 col-lg-4">
+              <article className="post-boxed">
+                <div className="post-meta">
+                  <div className="post-meta-item">
+                    <div className="post-author">
+                      <span>by</span>{" "}
+                      <a href="single-post.html">Theresa Simpson</a>
+                    </div>
+                  </div>
+                  <div className="post-meta-item">
+                    <div className="post-date">2 days ago</div>
+                  </div>
+                </div>
+                <a className="media-wrapper" href="single-post.html">
+                  <img src={i3} alt width={370} height={272} />
+                </a>
+                <div className="post-body">
+                  <ul className="list-tags">
+                    <li>
+                      <a className="tag-1" href="single-post.html">
+                        News
+                      </a>
+                    </li>
+                  </ul>
+                  <h5 className="post-title">
+                    <a href="single-post.html">
+                      Top 10 New Trends of Residential Interior Design
+                    </a>
+                  </h5>
+                  <p className="post-exeption">
+                    Vivamus fringilla accumsan nisi, non tempus lorem. Quisque
+                    non pretium risus. Maecenas pellentesque ipsum sit amet
+                    eleifend fermentumd duis quis.
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div className="col-md-6 col-lg-4">
+              <article className="post-boxed">
+                <div className="post-meta">
+                  <div className="post-meta-item">
+                    <div className="post-author">
+                      <span>by</span>{" "}
+                      <a href="single-post.html">Theresa Simpson</a>
+                    </div>
+                  </div>
+                  <div className="post-meta-item">
+                    <div className="post-date">2 days ago</div>
+                  </div>
+                </div>
+                <a className="media-wrapper" href="single-post.html">
+                  <img src={i4} alt width={370} height={272} />
+                </a>
+                <div className="post-body">
+                  <ul className="list-tags">
+                    <li>
+                      <a className="tag-1" href="single-post.html">
+                        News
+                      </a>
+                    </li>
+                  </ul>
+                  <h5 className="post-title">
+                    <a href="single-post.html">
+                      Curves And Round Shapes as Elements of Interior
+                    </a>
+                  </h5>
+                  <p className="post-exeption">
+                    Nullam venenatis, turpis vitae molestie vestibulum, magna
+                    ante lacinia erat, sed vestibulum augue massa at erat. Ut
+                    egestas purus quis placerat convallis.
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div className="col-md-6 col-lg-4">
+              <article className="post-boxed">
+                <div className="post-meta">
+                  <div className="post-meta-item">
+                    <div className="post-author">
+                      <span>by</span>{" "}
+                      <a href="single-post.html">Lawrence Kelly</a>
+                    </div>
+                  </div>
+                  <div className="post-meta-item">
+                    <div className="post-date">2 days ago</div>
+                  </div>
+                </div>
+                <a className="media-wrapper" href="single-post.html">
+                  <img src={i5} alt width={370} height={272} />
+                </a>
+                <div className="post-body">
+                  <ul className="list-tags">
+                    <li>
+                      <a className="tag-1" href="single-post.html">
+                        News
+                      </a>
+                    </li>
+                  </ul>
+                  <h5 className="post-title">
+                    <a href="single-post.html">
+                      Stylish Lighting: Brokis Balloon Light
+                    </a>
+                  </h5>
+                  <p className="post-exeption">
+                    Duis non urna metus. Praesent in massa at eros convallis
+                    finibus dignissim id urna. Donec volutpat nulla nisi, ut
+                    scelerisque ante elementum eget. Nunc egestas.
+                  </p>
+                </div>
+              </article>
+            </div>
+            <div className="col-md-6 col-lg-4">
+              <article className="post-boxed">
+                <div className="post-meta">
+                  <div className="post-meta-item">
+                    <div className="post-author">
+                      <span>by</span>{" "}
+                      <a href="single-post.html">Theresa Simpson</a>
+                    </div>
+                  </div>
+                  <div className="post-meta-item">
+                    <div className="post-date">2 days ago</div>
+                  </div>
+                </div>
+                <a className="media-wrapper" href="single-post.html">
+                  <img src={i6} alt width={370} height={272} />
+                </a>
+                <div className="post-body">
+                  <ul className="list-tags">
+                    <li>
+                      <a className="tag-1" href="single-post.html">
+                        News
+                      </a>
+                    </li>
+                  </ul>
+                  <h5 className="post-title">
+                    <a href="single-post.html">
+                      Moroccan Vibes With Yonder Living
+                    </a>
+                  </h5>
+                  <p className="post-exeption">
+                    Aenean tincidunt nisi magna, sed finibus urna facilisis nec.
+                    Nulla imperdiet, lectus vitae facilisis blandit, leo elit
+                    fermentum metus, et eleifend tellus nisl nec.
+                  </p>
+                </div>
+              </article>
+            </div>
+          </div>
+          {}
+          <nav className="nav-pagination" aria-label="Page navigation">
+            <ul className="pagination">
+              <li className="page-item page-item-control disabled">
+                <a className="page-link" aria-label="Previous">
+                  <span className="icon" aria-hidden="true" />
+                </a>
+              </li>
+              <li className="page-item active">
+                <span className="page-link">1</span>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  2
+                </a>
+              </li>
+              <li className="page-item page-item-control">
+                <a className="page-link" aria-label="Next">
+                  <span className="icon" aria-hidden="true" />
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default News;
